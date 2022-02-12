631fd2038d67c0d35d6fdf5eb653a3d0313ddcb0
huangdongbiao <dongbiao.huang@zhenai.com>
2022-02-12 15:03:18 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+
+import { ref, onMounted } from "vue";
+import axios from "axios";
+
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+const users = ref([]);
+onMounted(() => {
+    axios.get("http://localhost:3000/api/users").then((res) => {
+        console.log(res);
+        users.value = res.data;
+    });
+});
+</script>
+
+<template>
+    <ul>
+        <li v-for="item in users" :key="item.id">
+            用户ID：{{ item.id }}，用户名：{{ item.name }}
+        </li>
+    </ul>
+</template>
+
+<style>
+</style>
